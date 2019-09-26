34e5a9d0cef571fd636ed7b226e2caf31ff6217b
afleones <afleones@misena.edu.co>
2019-09-26 16:16:35 -0500

    Manejador de version de Ruby, Precompilar archivos para heroku.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Uniformate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.initialize_on_precompile = false
     #config.i18n.default_locale = :es
 
     # Settings in config/environments/* take precedence over those specified here.
