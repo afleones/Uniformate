edf0e209d552b760ab639142ad1238725222443f
James Giraldo <jamesgiiraldo@gmail.com>
2019-12-04 00:11:10 -0500

    listo coletasos bajen cambios y trabajen cares de vergas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to articulos_path()
+    end
+    unless user_signed_in?
+      redirect_to new_user_session_path()
+    end
   end
-  
 end
